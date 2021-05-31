97139287416261a44d0e64e4fdd03bc8f09fee75
Jeff Huffman <tejing@tejing.com>
2021-05-30 23:16:03 -0400

    add default gpg signing key

--- users/tejing/home.nix
+++ users/tejing/home.nix
@@ -288,6 +288,7 @@
                 }'';
 
   programs.gpg.enable = true;
+  programs.gpg.settings.default-key = "44A9 1F6C 152D ADE9 53BD  F9CE DE98 7C7E 445F 1961";
   services.gpg-agent.enable = true;
   
   programs.starship.enable = true;
