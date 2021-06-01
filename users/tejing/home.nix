24e5a0b39240376700752f8e8461c5b66a67c6f1
Jeff Huffman <tejing@tejing.com>
2021-05-31 21:21:43 -0400

    enable/configure dunst service

--- users/tejing/home.nix
+++ users/tejing/home.nix
@@ -149,7 +149,6 @@
     };
     startup = [
       { command = "${pkgs.feh}/bin/feh --no-fehbg --bg-fill '${pkgs.plasma-workspace-wallpapers}/share/wallpapers/Path/contents/images/2560x1440.jpg'"; always = true; notification = false; }
-      { command = "${pkgs.bash}/bin/bash -c \"${pkgs.killall}/bin/killall -w dunst; exec ${pkgs.dunst}/bin/dunst\""; always = true; notification = false; }
       { command = "${pkgs.xorg.xinput}/bin/xinput set-prop \"Logitech USB-PS/2 Optical Mouse\" \"libinput Accel Speed\" 0.6"; always = true; notification = false; }
       { command = "${pkgs.xss-lock}/bin/xss-lock -- ${pkgs.i3lock}/bin/i3lock -n -c 000000"; always = false; notification = false; }
       { command = "${pkgs.brave}/bin/brave"; always = false; notification = false; }
@@ -275,6 +274,62 @@
   services.picom.backend = "xrender";
   services.picom.vSync = true;
   services.picom.opacityRule = [ "0:_NET_WM_STATE@:32a *= '_NET_WM_STATE_HIDDEN'" ];
+
+  services.dunst.enable = true;
+  services.dunst.settings = {
+    global = {
+      font = "Iosevka Term 11";
+      markup = true;
+      plain_text = false;
+      format = "<b>%s</b>\\n%b";
+      sort = false;
+      alignment = "center";
+      bounce_freq = 0;
+      word_wrap = true;
+      hide_duplicate_count = true;
+      geometry = "384x5+0-0";
+      history_length = 15;
+      line_height = 3;
+      padding = 6;
+      horizontal_padding = 6;
+      separator_color = "frame";
+      startup_notification = true;
+      dmenu = "${pkgs.dmenu}/bin/dmenu";
+      browser = "${pkgs.brave}/bin/brave";
+      icon_position = "left";
+      max_icon_size = 80;
+      frame_width = 3;
+      frame_color = "#8EC07C";
+    };
+    # DEPRECATED: define keybindings and should be handled through i3 calling dunstctl
+    shortcuts = {
+      close = "ctrl+space";
+      close_all = "ctrl+shift+space";
+      history = "ctrl+grave";
+      context = "ctrl+shift+period";
+    };
+    urgency_low = {
+      frame_color = "#3B7C87";
+      foreground = "#3B7C87";
+      background = "#191311";
+      timeout = 4;
+    };
+    urgency_normal = {
+      frame_color = "#5B8234";
+      foreground = "#5B8234";
+      background = "#191311";
+      timeout = 6;
+    };
+    urgency_critical = {
+      frame_color = "#B7472A";
+      foreground = "#B7472A";
+      background = "#191311";
+      timeout = 8;
+    };
+    fullscreen = {
+      fullscreen = "pushback";
+    };
+  };
 
   programs.emacs.enable = true;
   programs.emacs.extraPackages = (epkgs: with epkgs; [ nix-mode haskell-mode ledger-mode ]);
