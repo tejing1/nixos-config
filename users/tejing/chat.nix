b79b6814be2e06e19226044aa15474da43e8d32c
Jeff Huffman <tejing@tejing.com>
2021-06-02 04:34:45 -0400

    add workspace assignment for element webapp

--- users/tejing/chat.nix
+++ users/tejing/chat.nix
@@ -4,7 +4,8 @@
   home.packages = with pkgs; [
     weechat
   ];
-  xsession.windowManager.i3.config.assigns."10" = [{ class = "^Brave-browser$"; instance = "^discord.com__app$"; }];
+  xsession.windowManager.i3.config.assigns."10" = [{ class = "^Brave-browser$"; instance = "^discord.com__app$"; }
+                                                   { class = "^Brave-browser$"; instance = "^app.element.io$";   }];
   xsession.windowManager.i3.config.assigns."11" = [{ class = "^URxvt$"; instance = "^weechat$"; }];
   xsession.windowManager.i3.config.startup = [{ command = "${pkgs.rxvt-unicode}/bin/urxvtc -name weechat -e weechat"; always = false; notification = false; }];
 }
