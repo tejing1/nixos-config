94b58c730d12211d548e189cbeec625fa3bf7947
Jeff Huffman <tejing@tejing.com>
2021-06-08 08:31:33 -0400

    set new default gpg key

--- users/tejing/encryption.nix
+++ users/tejing/encryption.nix
@@ -2,6 +2,6 @@
 
 {
   programs.gpg.enable = true;
-  programs.gpg.settings.default-key = "44A9 1F6C 152D ADE9 53BD  F9CE DE98 7C7E 445F 1961";
+  programs.gpg.settings.default-key = "963D 3AFB 8AA4 D693 153C  1500 46E9 6F6F F44F 3D74";
   services.gpg-agent.enable = true;
 }
