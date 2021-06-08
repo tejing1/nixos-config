564dd2512b08a89e4574a6ee0776c044c0918554
Jeff Huffman <tejing@tejing.com>
2021-06-08 08:42:49 -0400

    set default recipient to match my default key

--- users/tejing/encryption.nix
+++ users/tejing/encryption.nix
@@ -3,5 +3,6 @@
 {
   programs.gpg.enable = true;
   programs.gpg.settings.default-key = "963D 3AFB 8AA4 D693 153C  1500 46E9 6F6F F44F 3D74";
+  programs.gpg.settings.default-recipient-self = true;
   services.gpg-agent.enable = true;
 }
