7689968d223b697fe7ba30406684b46f3279013a
Jeff Huffman <tejing@tejing.com>
2021-06-08 20:34:31 -0400

    disable iso14755 support un urxvt. ctrl+shift is just too disruptive to bind, especially in emacs

--- users/tejing/xterminal.nix
+++ users/tejing/xterminal.nix
@@ -10,6 +10,7 @@
   programs.urxvt.fonts = [ "xft:DejaVuSansMono Nerd Font Mono:pixelsize=15" ];
   programs.urxvt.scroll.bar.enable = false;
   programs.urxvt.scroll.lines = 0;
+  programs.urxvt.iso14755 = false;
   programs.urxvt.extraConfig = {
     background = "rgba:0000/0000/0000/C000";
     foreground = "#00ff00";
