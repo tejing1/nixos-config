164f80b2f4d44a950d0fc84f9977bfc2cae8af89
Jeff Huffman <tejing@tejing.com>
2021-06-10 06:54:04 -0400

    install git-remote-gcrypt for backing up my password-store

--- users/tejing/encryption.nix
+++ users/tejing/encryption.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }:
 
 {
+  home.packages = with pkgs; [
+    git-remote-gcrypt
+  ];
   programs.gpg.enable = true;
   programs.gpg.settings.default-key = "963D 3AFB 8AA4 D693 153C  1500 46E9 6F6F F44F 3D74";
   programs.gpg.settings.default-recipient-self = true;
