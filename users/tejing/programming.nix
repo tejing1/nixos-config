5d312a8e1ac3e9d7e42a8e4bb5783cc67baaf43b
Jeff Huffman <tejing@tejing.com>
2021-06-10 06:28:55 -0400

    remove space from git.signingKey because git-remote-gcrypt barfs

--- users/tejing/programming.nix
+++ users/tejing/programming.nix
@@ -12,7 +12,7 @@
   programs.git.userName = "Jeff Huffman";
   programs.git.userEmail = "tejing@tejing.com";
   programs.git.ignores = [ "*~" ".#*" "\\#*#" ];
-  programs.git.signing.key = "963D 3AFB 8AA4 D693 153C  1500 46E9 6F6F F44F 3D74";
+  programs.git.signing.key = "963D3AFB8AA4D693153C150046E96F6FF44F3D74";
   programs.git.signing.signByDefault = true;
   programs.git.extraConfig.tag.gpgSign = true;
   programs.git.extraConfig.push.gpgSign = "if-asked";
