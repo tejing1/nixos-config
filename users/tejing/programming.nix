c0ac3ab2e3f69992f2a5a1f5012f9049bb1bac6f
Jeff Huffman <tejing@tejing.com>
2021-06-09 20:05:03 -0400

    configure git with home-manager

--- users/tejing/programming.nix
+++ users/tejing/programming.nix
@@ -1,12 +1,19 @@
 { pkgs, ... }:
 
 {
-  # perhaps this should be left to individual development environments?
   home.packages = with pkgs; [
-    git
     github-cli
     coq
+    # perhaps this should be left to individual development environments?
     ruby
     ghc
   ];
+  programs.git.enable = true;
+  programs.git.userName = "Jeff Huffman";
+  programs.git.userEmail = "tejing@tejing.com";
+  programs.git.ignores = [ "*~" ".#*" "\\#*#" ];
+  programs.git.signing.key = "963D 3AFB 8AA4 D693 153C  1500 46E9 6F6F F44F 3D74";
+  programs.git.signing.signByDefault = true;
+  programs.git.extraConfig.tag.gpgSign = true;
+  programs.git.extraConfig.push.gpgSign = "if-asked";
 }
