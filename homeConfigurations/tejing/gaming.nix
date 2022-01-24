634f6146b54aff4b0334b73d8284af00952f1a69
Jeff Huffman <tejing@tejing.com>
2022-01-24 18:48:56 -0500

    add stardew valley deps to steam FHS

--- homeConfigurations/tejing/gaming.nix
+++ homeConfigurations/tejing/gaming.nix
@@ -1,11 +1,24 @@
 { pkgs, ... }:
 
+let
+  mySteam = pkgs.steam.override {
+    extraPkgs = pkgs: builtins.attrValues {
+      inherit (pkgs)
+
+        # stardew valley
+        icu
+
+      ;
+    };
+  };
+in
 {
   home.packages = builtins.attrValues {
     inherit (pkgs)
-      steam
       lutris
     ;
+    inherit mySteam;
+    steam-run = mySteam.run;
   };
   xsession.windowManager.i3.config.assigns."8" = [{class = "^Steam$";}];
 }
