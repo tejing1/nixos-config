6c9bec5ed1298a793f9c1fd05066f17705b5dc19
Jeff Huffman <tejing@tejing.com>
2022-02-08 11:32:49 -0500

    set window titles from zsh

--- homeConfigurations/tejing/shell.nix
+++ homeConfigurations/tejing/shell.nix
@@ -114,6 +114,38 @@
     . ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
     [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
     . ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+    # Display $1 in terminal title.
+    function set-term-title() {
+      emulate -L zsh
+      if [[ -t 1 ]]; then
+        print -rn -- $'\e]0;'${"$"}{(V)1}$'\a'
+      elif [[ -w $TTY ]]; then
+        print -rn -- $'\e]0;'${"$"}{(V)1}$'\a' >$TTY
+      fi
+    }
+
+    # When a command is running, display it in the terminal title.
+    function set-term-title-preexec() {
+      if (( P9K_SSH )); then
+        set-term-title ''${(V%):-"%n@%m: "}$1
+      else
+        set-term-title $1
+      fi
+    }
+
+    # When no command is running, display the current directory in the terminal title.
+    function set-term-title-precmd() {
+      if (( P9K_SSH )); then
+        set-term-title ''${(V%):-"%n@%m: %~"}
+      else
+        set-term-title ''${(V%):-"%~"}
+      fi
+    }
+
+    autoload -Uz add-zsh-hook
+    add-zsh-hook preexec set-term-title-preexec
+    add-zsh-hook precmd set-term-title-precmd
   '';
 
   programs.starship.enable = true;
