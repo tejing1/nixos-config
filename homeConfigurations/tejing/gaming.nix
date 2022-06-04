fe781d85e83952562e532593937ae4b6d5130f2a
Jeff Huffman <tejing@tejing.com>
2022-06-04 14:49:06 -0400

    use steam PR and programs.steam.enable nixos option

--- homeConfigurations/tejing/gaming.nix
+++ homeConfigurations/tejing/gaming.nix
@@ -1,29 +1,10 @@
 { nixosConfig, pkgs, ... }:
 
-let
-  mySteam = pkgs.steam.override {
-    extraPkgs = pkgs: builtins.attrValues {
-      inherit (pkgs)
-
-        # stardew valley
-        icu
-
-      ;
-    };
-
-    # copied from nixos steam module
-    extraLibraries = pkgs:
-      let inherit (nixosConfig.hardware.opengl) package package32 extraPackages extraPackages32; in
-      if pkgs.hostPlatform.is64bit then [ package ] ++ extraPackages else [ package32 ] ++ extraPackages32;
-  };
-in
 {
   home.packages = builtins.attrValues {
     inherit (pkgs)
       lutris
     ;
-    inherit mySteam;
-    steam-run = mySteam.run;
   };
 
   # prevent compositing performance hit when gaming
---
+++ nixosConfigurations/tejingdesk/gaming.nix
@@ -0,0 +1,23 @@
+{ my, pkgs, ...}:
+
+let
+  steam-pr-overlay = final: prev: {
+    steam = (
+      import (
+        final.fetchFromGitHub {
+          owner = "jonringer";
+          repo = "nixpkgs";
+          rev = "d536e0a0eb54ea51c676869991fe5a1681cc6302";
+          sha256 = "sha256-733fLqgkYUPK7jQxXkQihzihkiTtQG/vcV/ZyuDz86Y=";
+        }
+      ) {
+        inherit (final) system;
+        config.allowUnfree = true;
+      }
+    ).steam;
+  };
+in
+{
+  nixpkgs.overlays = [ steam-pr-overlay ];
+  programs.steam.enable = true;
+}
