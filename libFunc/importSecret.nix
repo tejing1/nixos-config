542750dcdbf3d62d80d4f24d71f0b2b8c9f70b72
Jeff Huffman <tejing@tejing.com>
2022-07-26 01:29:36 -0400

    reimplement {read,import}Secret to reduce IFD instances

--- libFunc/importSecret.nix
+++ libFunc/importSecret.nix
@@ -1,12 +1,5 @@
-{ pkgs, ... }:
-
-default: file:
+{ my, pkgs, ... }:
 let
-  # This should not be this hard, but readFile and nix strings just
-  # refuse to deal with nulls, so this is seems to be the only way to
-  # do this.
-  locked = import (pkgs.runCommand "test-result" { inherit file; } ''
-    if diff <(head -c10 "$file") <(echo -ne '\x00GITCRYPT\x00'); then echo true > $out; else echo false > $out; fi
-  '');
+  importSecret = if my.lib.isRepoLocked then default: file: default else default: file: import file;
 in
-if locked then default else import file
+importSecret
---
+++ libFunc/isRepoLocked/default.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+let
+  # This should not be this hard, but readFile and nix strings just
+  # refuse to deal with nulls, so this seems to be the only way to do
+  # it.
+  isRepoLocked = import (pkgs.runCommand "test-result" { file = ./dummy.secret; } ''
+    if diff <(head -c10 "$file") <(echo -ne '\x00GITCRYPT\x00'); then
+      # file starts with ^@GITCRYPT^@
+      echo true > $out
+    else
+      # file does not start with ^@GITCRYPT^@
+      echo false > $out
+    fi
+  '');
+in
+isRepoLocked
