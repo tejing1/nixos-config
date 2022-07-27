ba340afbe570baa61886d12d4f895673bb8ec921
Jeff Huffman <tejing@tejing.com>
2022-07-26 23:33:18 -0400

    remove instance of IFD

--- nixosConfigurations/tejingdesk/gaming.nix
+++ nixosConfigurations/tejingdesk/gaming.nix
@@ -1,16 +1,17 @@
 { my, pkgs, ...}:
 
 let
+  steam-pr = fetchTree {
+    type = "github";
+    owner = "jonringer";
+    repo = "nixpkgs";
+    rev = "d536e0a0eb54ea51c676869991fe5a1681cc6302";
+    narHash = "sha256-733fLqgkYUPK7jQxXkQihzihkiTtQG/vcV/ZyuDz86Y=";
+  };
+
   steam-pr-overlay = final: prev: {
     steam = (
-      import (
-        final.fetchFromGitHub {
-          owner = "jonringer";
-          repo = "nixpkgs";
-          rev = "d536e0a0eb54ea51c676869991fe5a1681cc6302";
-          sha256 = "sha256-733fLqgkYUPK7jQxXkQihzihkiTtQG/vcV/ZyuDz86Y=";
-        }
-      ) {
+      import steam-pr {
         inherit (final) system;
         config.allowUnfree = true;
       }
@@ -18,6 +19,17 @@ let
   };
 in
 {
-  nixpkgs.overlays = [ steam-pr-overlay my.overlays.steam-fix-screensaver ];
+  nixpkgs.overlays = [
+    # use steam from jonringer's pr rather than main nixpkgs
+    steam-pr-overlay
+
+    # keep steam from inhibiting the screensaver
+    my.overlays.steam-fix-screensaver
+  ];
+
   programs.steam.enable = true;
+
+  # Save the fetchTree result as a dependency of the system generation
+  # so we don't need to repeatedly download it.
+  system.extraDependencies = [ steam-pr ];
 }
