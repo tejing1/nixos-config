572cdee846698693e39d9448b7a56966ffffa1f3
Jeff Huffman <tejing@tejing.com>
2022-12-09 01:21:54 -0500

    auto-start nixos discourse

--- homeConfigurations/tejing/chat.nix
+++ homeConfigurations/tejing/chat.nix
@@ -10,11 +10,13 @@
     { command = "${my.launch.term} app weechat ${pkgs.weechat}/bin/weechat"; always = false; notification = false; }
     { command = "${my.pwarun} https://discord.com/app";always = false; notification = false; }
     { command = "${my.pwarun} https://app.element.io";always = false; notification = false; }
+    { command = "${my.pwarun} https://discourse.nixos.org/latest";always = false; notification = false; }
   ];
   xsession.windowManager.i3.config.window.commands = [{ criteria = { class = "^URxvt$"; instance = "^weechat$"; }; command = "layout tabbed"; }];
   xsession.windowManager.i3.config.assigns."11" = [
     { class = "^Brave-browser$"; instance = "^discord.com__app$"; }
-    { class = "^Brave-browser$"; instance = "^app.element.io$";   }
+    { class = "^Brave-browser$"; instance = "^app.element.io$"; }
+    { class = "^Brave-browser$"; instance = "^discourse.nixos.org__latest$"; }
     { class = "^URxvt$"; instance = "^weechat$"; }
   ];
 }
