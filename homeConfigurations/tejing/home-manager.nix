440adc8d1991ca94252f7abe204ec64b6c34dd76
Jeff Huffman <tejing@tejing.com>
2023-01-19 21:10:31 -0500

    auto-clean home-manager profile

--- homeConfigurations/tejing/home-manager.nix
+++ homeConfigurations/tejing/home-manager.nix
@@ -1,6 +1,19 @@
+{ config, nixosConfig, ... }:
+
 {
   # Automatically (re)start/stop and changed services when activating a home-manager configuration.
   systemd.user.startServices = true;
+
+  systemd.user.services.clean_hm_profile = {
+    Unit.Description = "home-manager profile cleaner";
+    Service.ExecStart = "${nixosConfig.nix.package}/bin/nix profile wipe-history --profile /nix/var/nix/profiles/per-user/${config.home.username}/home-manager";
+  };
+  systemd.user.timers.clean_hm_profile = {
+    Unit.Description = "home-manager profile cleaner timer";
+    Install.WantedBy = [ "timers.target" ];
+    Timer.OnCalendar = "daily";
+    Timer.Persistent = true;
+  };
 
   home.stateVersion = "20.09";
 }
