c3548e5265e9bf30ebb87acfb12bf45b7451c811
Jeff Huffman <tejing@tejing.com>
2023-06-06 00:35:56 -0400

    update to nixos 23.05

--- nixosConfigurations/tejingdesk/ssh.nix
+++ nixosConfigurations/tejingdesk/ssh.nix
@@ -1,7 +1,7 @@
 {
   services.openssh.enable = true;
-  services.openssh.permitRootLogin = "no";
-  services.openssh.passwordAuthentication = false;
+  services.openssh.settings.PermitRootLogin = "no";
+  services.openssh.settings.PasswordAuthentication = false;
   services.openssh.hostKeys = [
     { type = "rsa"; bits = 4096; path = "/mnt/persist/tejingdesk/ssh_host_keys/ssh_host_rsa_key"; }
     { type = "ed25519";          path = "/mnt/persist/tejingdesk/ssh_host_keys/ssh_host_ed25519_key"; }
--- nixosConfigurations/tejingdesk/xserver.nix
+++ nixosConfigurations/tejingdesk/xserver.nix
@@ -10,39 +10,6 @@
   nixpkgs.config.allowUnfree = true;
   services.xserver.videoDrivers = [ "nvidia" ];
 
-  # Fix segfault on login: NixOS/nixpkgs#206663
-  nixpkgs.overlays = [ (final: prev: {
-    xorg = prev.xorg.overrideScope (xfinal: xprev: {
-      xorgserver = xprev.xorgserver.overrideAttrs (oldAttrs: {
-        patches = oldAttrs.patches or [] ++ [ (builtins.toFile "xserver-segfault-fix.patch" ''
-          diff --git a/present/present_scmd.c b/present/present_scmd.c
-          index da836ea6b..239055bc1 100644
-          --- a/present/present_scmd.c
-          +++ b/present/present_scmd.c
-          @@ -158,6 +158,9 @@ present_scmd_get_crtc(present_screen_priv_ptr screen_priv, WindowPtr window)
-               if (!screen_priv->info)
-                   return NULL;
-
-          +    if (!screen_priv->info->get_crtc)
-          +        return NULL;
-          +
-               return (*screen_priv->info->get_crtc)(window);
-           }
-
-          @@ -196,6 +199,9 @@ present_flush(WindowPtr window)
-               if (!screen_priv->info)
-                   return;
-
-          +    if (!screen_priv->info->flush)
-          +        return;
-          +
-               (*screen_priv->info->flush) (window);
-           }
-        '') ];
-      });
-    });
-  }) ];
-
   # Hardware-accelerated video decoding
   hardware.opengl.extraPackages = builtins.attrValues {
     inherit (pkgs)
