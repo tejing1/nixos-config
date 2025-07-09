cbd421011c8538eaca008f6f078cfd439c39f07a
Jeff Huffman <tejing@tejing.com>
2025-07-09 05:06:03 -0400

    convert homeModules, remove legacyOutputs

--- default.nix
+++ default.nix
@@ -3,21 +3,11 @@
 # Note: Unlike in many repos, this file isn't intended as an entry
 # point for nix-build. It's a flake-parts module.
 
-let
-  inherit (builtins) attrNames;
-
-  legacyOutputs = {
-    homeModules = import ./homeModules inputs;
-  };
-in
-
 {
-  imports = mylib.listImportablePathsExcept ./. ([
+  imports = mylib.listImportablePathsExcept ./. [
     "flake"
     "default"
-  ] ++ attrNames legacyOutputs);
-
-  flake = legacyOutputs;
+  ];
 
   # TODO: Put this somewhere better
   systems = [
