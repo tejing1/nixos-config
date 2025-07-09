6e2cceddfaefd4b992ee5b5f44c287ccb5ba6627
Jeff Huffman <tejing@tejing.com>
2025-07-09 00:57:28 -0400

    import top-level files as flake-parts modules unless otherwise specified

--- default.nix
+++ default.nix
@@ -3,10 +3,32 @@
 # Note: Unlike in many repos, this file isn't intended as an entry
 # point for nix-build. It's a flake-parts module.
 
-{
-  imports = [ ./lib ];
+let
+  inherit (builtins) attrNames;
 
-  flake = mylib.importAllExceptWithArg ./. [ "flake" "lib" "default" ] inputs;
+  legacyOutputs = {
+    apps = import ./apps inputs;
+    homeConfigurations = import ./homeConfigurations inputs;
+    homeModules = import ./homeModules inputs;
+    inputs = import ./inputs.nix inputs;
+    libFor = import ./libFor.nix inputs;
+    libFunc = import ./libFunc inputs;
+    nixosConfigurations = import ./nixosConfigurations inputs;
+    nixosModules = import ./nixosModules inputs;
+    overlays = import ./overlays inputs;
+    packages = import ./packages.nix inputs;
+    packagesFunc = import ./packagesFunc inputs;
+    packagesUnstable = import ./packagesUnstable.nix inputs;
+  };
+in
+
+{
+  imports = mylib.listImportablePathsExcept ./. ([
+    "flake"
+    "default"
+  ] ++ attrNames legacyOutputs);
+
+  flake = legacyOutputs;
 
   # TODO: Put this somewhere better
   systems = [
